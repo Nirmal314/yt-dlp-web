9cb77ecf06dfc70ef0be905ca94a176cd7f26c22
Nirmal Ambasana <nambasana033@gmail.com>
2025-04-01 17:30:08 +0530

    Add check()

--- lib/ytdlp.ts
+++ lib/ytdlp.ts
@@ -2,4 +2,13 @@ import { YtDlp } from "ytdlp-nodejs";
 
 const ytdlp = new YtDlp();
 
+const check = async () => {
+  const isInstalled = await ytdlp.checkInstallationAsync({ ffmpeg: true });
+
+  if (!isInstalled) {
+    // TODO: install
+    const res = await ytdlp.downloadFFmpeg();
+  }
+};
+export { check };
 export default ytdlp;
